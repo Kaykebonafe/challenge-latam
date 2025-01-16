08e18c3219bdfd9bb599a8539d410f9235683eb1
Kayke B. Kakazu <kayke.bonafe98@gmail.com>
2025-01-16 05:36:07 -0300

    feat: model implemented and tested

--- challenge/model.py
+++ challenge/model.py
@@ -1,19 +1,40 @@
 import pandas as pd
+import numpy as np
 
 from typing import Tuple, Union, List
+from joblib import dump, load
+from datetime import datetime
+from sklearn.linear_model import LogisticRegression
+
 
 class DelayModel:
+    _FEATURE_COLUMNS = [
+        "OPERA_Latin American Wings",
+        "MES_7",
+        "MES_10",
+        "OPERA_Grupo LATAM",
+        "MES_12",
+        "TIPOVUELO_I",
+        "MES_4",
+        "MES_11",
+        "OPERA_Sky Airline",
+        "OPERA_Copa Air"
+    ]
+
+    @property
+    def get_feature_cols(self):
+        return self._FEATURE_COLUMNS
 
     def __init__(
         self
     ):
-        self._model = None # Model should be saved in this attribute.
+        self._model = LogisticRegression() # Model should be saved in this attribute.
 
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -26,7 +47,32 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        data['Fecha-O'] = pd.to_datetime(data['Fecha-O'], format='%Y-%m-%d %H:%M:%S')
+        data['Fecha-I'] = pd.to_datetime(data['Fecha-I'], format='%Y-%m-%d %H:%M:%S')
+
+        data['min_diff'] = (data['Fecha-O'] - data['Fecha-I']).dt.total_seconds() / 60
+
+        threshold_in_minutes = 15
+        data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
+
+        if target_column:
+            target = pd.DataFrame(data[target_column])
+
+        features = pd.concat(
+            [
+                pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
+                pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'),
+                pd.get_dummies(data['MES'], prefix = 'MES')
+            ],
+            axis = 1
+        )
+
+        features = features[self.get_feature_cols]
+
+        if target_column:
+            return (features, target)
+        else:
+            return features
 
     def fit(
         self,
@@ -40,7 +86,17 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+
+        n_y0 = len(target[target["delay"] == 0])
+        n_y1 = len(target[target["delay"] == 1])
+
+        class_weight = {1: n_y0/len(target), 0: n_y1/len(target)}
+        self._model.class_weight = class_weight
+
+        self._model.fit(features, target)
+
+        dump(self._model, "flight_delay_logreg_model.joblib")
+
 
     def predict(
         self,
@@ -51,8 +107,13 @@ class DelayModel:
 
         Args:
             features (pd.DataFrame): preprocessed data.
-        
+
         Returns:
             (List[int]): predicted targets.
         """
-        return
+
+        self._model = load("flight_delay_logreg_model.joblib")
+
+        prediction = self._model.predict(features)
+
+        return prediction.tolist()
